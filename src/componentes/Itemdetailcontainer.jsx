6cfd15ee10e9cf69ec12d44e3a85ef1dce8ed00b
unknown <trivisonnolucas@gmail.com>
2022-09-28 15:19:52 -0300

    firebase

--- src/componentes/Itemdetailcontainer.jsx
+++ src/componentes/Itemdetailcontainer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
@@ -10,25 +11,13 @@ const Itemdetailcontainer = () => {
     
 
     useEffect(() => {
-        const productos =[
-            {"id": 1, "nombre":"Doble Cuarto XL", "imagen":"https://s3-eu-central-1.amazonaws.com/www.burgerking.com.ar.v2/wp-media-folder-bk-argentina//home/ubuntu/preview/menu-app/frontend/apps/marketing-website-wordpress-app/web/app/uploads/sites/5/BK_WEB_DOBLE-CUARTO-XL_1200X800_020822.png","descripcion":"Dos carnes a la parrilla, pan, queso cheddar, cebolla, mostaza y ketchup." ,"precio": 900},
-            {"id": 2, "nombre":"Bacon King Carne", "imagen":"https://s3-eu-central-1.amazonaws.com/www.burgerking.com.ar.v2/wp-media-folder-burger-king-argentina//home/ubuntu/preview/menu-app/frontend/apps/marketing-website-wordpress-app/web/app/uploads/sites/5/Bacon-King-Carne.png","descripcion":"Dos medallones de carne, pan, queso cheddar, panceta, ketchup y mostaza.  " ,"precio": 1000},
-            {"id": 3, "nombre":"Stacker XL Triple", "imagen":"https://s3-eu-central-1.amazonaws.com/www.burgerking.com.ar.v2/wp-media-folder-bk-argentina//home/ubuntu/preview/menu-app/frontend/apps/marketing-website-wordpress-app/web/app/uploads/sites/5/BK_WEB_STACKER-XL-_1200X800_020822.png","descripcion":"Tres carnes a la parrilla, salsa stacker, pan, queso cheddar, panceta" ,"precio":1000},
-            {"id": 4, "nombre":"Chimiburga Carne", "imagen":"https://s3-eu-central-1.amazonaws.com/www.burgerking.com.ar.v2/wp-media-folder-burger-king-argentina//home/ubuntu/preview/menu-app/frontend/apps/marketing-website-wordpress-app/web/app/uploads/sites/5/Chimiburga_Carne.png","descripcion":"Disfrutá esta tremenda hamburguesa con pan, tomate, cebolla, lechuga, mayonesa y el protagonista: CHIMICHURRI. Y sí, la podés pedir en versión vegetal."
-            ,"precio":900},
-            {"id": 5, "nombre":"Doble Napolitano XL", "imagen":"https://s3-eu-central-1.amazonaws.com/www.burgerking.com.ar.v2/wp-media-folder-burger-king-argentina//home/ubuntu/preview/menu-app/frontend/apps/marketing-website-wordpress-app/web/app/uploads/sites/5/DobleNapolitano-XL-1.png","descripcion":"La combinación de siempre, ahora duplicada: 2 carnes a la parrilla + jamón y queso + tomate + mayonesa."
-             ,"precio":900},
-            {"id": 6, "nombre":"Wow Camole", "imagen":"https://s3-eu-central-1.amazonaws.com/www.burgerking.com.ar.v2/wp-media-folder-burger-king-argentina//home/ubuntu/preview/menu-app/frontend/apps/marketing-website-wordpress-app/web/app/uploads/sites/5/WowCamole_carne_300322.png","descripcion":"Una combinación de carne a la parrilla, queso pategrás, tomate, cebolla, rúcula, guacamole y mayonesa con pan de quínoa que te va a dejar sin palabras." ,"precio":900}
-        ];
-
-        const getProductos = new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(productos.find(producto => producto.id === parseInt(id)) || []);
-            }, 1000);
-        });
-
-        getProductos.then((respuesta) => {
-            setItem(respuesta);
+        const db = getFirestore();
+        const response = doc(db, "items", id);
+        getDoc(response).then((snapShot) => {
+            if (snapShot.exists()) {
+                setItem({id:snapShot.id, ...snapShot.data()});
+               
+            }            
         });
     }, [id]);
     return (
--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3mZrFbkcL9Cv-WvYHPurmIv5tQgk03Lk",
+  authDomain: "coderhouse-ecommerce-930f5.firebaseapp.com",
+  projectId: "coderhouse-ecommerce-930f5",
+  storageBucket: "coderhouse-ecommerce-930f5.appspot.com",
+  messagingSenderId: "73279078924",
+  appId: "1:73279078924:web:f36c6a77e0471ec9ab6429"
+};
+
+// Initialize Firebase
+ initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
