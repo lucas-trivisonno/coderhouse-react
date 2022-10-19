9b9992ef8af2f71879a724050db9357b0547946b
unknown <trivisonnolucas@gmail.com>
2022-10-18 21:06:40 -0300

    proyecto final

--- src/componentes/Comprafinalizada.jsx
+++ src/componentes/Comprafinalizada.jsx
@@ -8,7 +8,7 @@ const Comprafinalizada = ({id}) => {
             <div className="col-md-12 text-center p-5">
                 <h1>Felicitaciones!</h1>
                 <p  width="180">Tu compra se ha realizado con exito</p>
-                <p>El pedidio se generó con el ID: <b>{id}</b></p>
+                <p>El pedido se generó con el ID: <b>{id}</b></p>
                 <p><b>Disfrute su comida!!!</b> </p>
                 <Link to={"/productos"}><button className="btn btn-danger">Volver al menu</button></Link>
             </div>
